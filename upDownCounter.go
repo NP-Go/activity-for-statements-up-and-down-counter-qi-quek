fc6c41205e825f7b45f300afd2528759b9ed8066
narwhal04 <q.quek7@gmail.com>
2022-03-31 16:12:17 +0800

    functioning code

--- upDownCounter.go
+++ upDownCounter.go
@@ -1,5 +1,26 @@
 package main
 
+import "fmt"
+
+var numberBig, numberSmall int
+
 func main() {
-	//Insert code here
+	fmt.Println("Please input two numbers")
+	fmt.Println("Input first number")
+	fmt.Scan(&numberBig)
+	fmt.Println("Input second number")
+	fmt.Scan(&numberSmall)
+
+	if numberBig < numberSmall {
+		numberBig, numberSmall = numberSmall, numberBig
+	}
+
+	for noAscend := numberSmall; noAscend <= numberBig; noAscend++ {
+		fmt.Println(noAscend)
+	}
+	// status = false
+	for noDescedng := numberBig; noDescedng >= numberSmall; noDescedng-- {
+		fmt.Println(noDescedng)
+	}
+
 }
